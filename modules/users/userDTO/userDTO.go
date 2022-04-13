cf9283cc717f5fc9228f8fe6ed7c0266c2c14575
muhammadnurbasari <m.nurbasari@gmail.com>
2022-04-14 00:57:29 +0700

    update DTO user

--- modules/users/userDTO/userDTO.go
+++ modules/users/userDTO/userDTO.go
@@ -11,38 +11,8 @@ type ResLoginResult struct {
 	Result    loginModel.ResLogin `json:"result"`
 }
 
-// ForbiddenRes - response forbidden request
-type ForbiddenRes struct {
-	Status    int    `json:"status" example:"403"`
-	Messagges string `json:"messages" example:"incorrect Email or Password"`
-}
-
-// ResRegister - response register account
-type ResRegister struct {
-	Status    int    `json:"status" example:"200"`
-	Messagges string `json:"messages" example:"Verification code send to your email, Please Verification On 2 Minute !"`
-	UserCode  string `json:"user_code" example:""`
-}
-
-//  ResVerificationOtp
-type ResVerificationOtp struct {
-	Status    int    `json:"status" example:"200"`
-	Messagges string `json:"messages" example:"Verification Success, Please Login !"`
-}
-
-//  ResResendOtp
-type ResResendOtp struct {
-	Status    int    `json:"status" example:"200"`
-	Messagges string `json:"messages" example:"Resend Success, Please Verification On 2 Minute !"`
-}
-
 //
 type ResNotFound struct {
 	Status    int    `json:"status" example:"404"`
-	Messagges string `json:"messages" example:"Please Registration Your Account !"`
-}
-
-type ResOK struct {
-	Status    int    `json:"status" example:"200"`
-	Messagges string `json:"messages" example:"success"`
+	Messagges string `json:"messages" example:"Wrong Username"`
 }
